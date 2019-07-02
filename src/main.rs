d1363ed99110681da4c16094c45a87207971deb7
Joshua Suskalo <joshua@suskalo.org>
2019-07-01 22:09:22 -0500

    Call assemble_file from main method based on args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use chip8_assembler::assemble_file;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    assemble_file(&args[1], &args[2]).unwrap();
 }
